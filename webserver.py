4e8db474d069e890f9ee04a93761652394eace88
ajtheeck <ajtheeck@protonmail.com>
2025-09-02 14:36:22 -0400

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -7,8 +7,9 @@ def home():
     return ("Discord bot okay")
 
 def run():
-    app.run(host= 0.0.0.0, port=8080)
+    app.run(host= "0.0.0.0", port=8080)
 
 def keep_alive():
     Thread(target=run)
+
     t.start()
